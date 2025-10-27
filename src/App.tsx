fa168ef72d9e80f7a5a7c2b275f66a2d720542a2
Karina Allard <karina.allard.92@gmail.com>
2025-10-27 12:08:36 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <main className='flex max-w-full justify-center m-10'>
+        <h1 className="text-8xl text-cyan-200">Friday Wheel</h1>
+        <section>
+
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
